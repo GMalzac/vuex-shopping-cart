adbc097aa2ba1714931c649dbbedf7ee52d5d506
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-04-23 16:28:56 +0100

    Initialize prodct list component

---
+++ src/components/ProductList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Products List</h1>
+    <ul>
+      <li v-for="product in products">
+        {{product.title}} - {{product.price}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'ProductList',
+
+  data () {
+    return {
+      products: []
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
